ef6927225f91591ba634e399c3e5dcda9051272b
Macusuweru <128636999+Macusuweru@users.noreply.github.com>
2025-01-20 15:35:04 -0700

    Add files via upload

--- CurrentPatientGeneric.java
+++ CurrentPatientGeneric.java
@@ -2,18 +2,18 @@ package assign02;
 
 import java.util.GregorianCalendar;
 
-public class CurrentPatient extends Patient{
-    int physician;
+public class CurrentPatientGeneric<Type> extends Patient{
+    Type physician;
     GregorianCalendar lastVisit;
     /**
-     * Creates a new CurrentPatient, which is a patient that also stores the associated physician's ID and the date of the patient's last visit.
+     * Creates a new CurrentPatientGeneric, which is a patient that also stores the associated physician's ID and the date of the patient's last visit. The type of the physician's ID must be specified.
      * @param firstName First name of the patient
      * @param lastName Last name of the patient
      * @param uHealthID uHealthID of the patient
      * @param physician physician's ID
      * @param lastVisit The date of the patient's last visit
      */
-    public CurrentPatient(String firstName, String lastName, UHealthID uHealthID, int physician, GregorianCalendar lastVisit) {
+    public CurrentPatientGeneric (String firstName, String lastName, UHealthID uHealthID, Type physician, GregorianCalendar lastVisit) {
         super(firstName, lastName, uHealthID);
         this.physician = physician;
         this.lastVisit = lastVisit;
@@ -22,7 +22,7 @@ public class CurrentPatient extends Patient{
      * Return the ID of the patient's physician
      * @return The ID
      */
-    public int getPhysician() {
+    public Type getPhysician() {
         return this.physician;
     }
     /**
@@ -36,7 +36,7 @@ public class CurrentPatient extends Patient{
      * Change the physician associated with this patient
      * @param newPhysician The ID of the new physician
      */
-    public void updatePhysician(int newPhysician) {
+    public void updatePhysician(Type newPhysician) {
         this.physician = newPhysician;
     }
 
